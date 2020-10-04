0f5a5ff94e166f1dc66b566cc8399f93b0d9fe52
udayan1410 <udayanbirajdar@gmail.com>
2020-10-03 22:24:35 -0700

    Created Constants and added navigation drawer (#3)
    * Login Done
    
    * Handled Error Condition for Login
    
    * Changed styling a bit for Regular button
    
    * Update ServerRoutes.js
    
    * Created Navigation drawer

--- room-ease/src/constants/ServerRoutes.js
+++ room-ease/src/constants/ServerRoutes.js
@@ -1,2 +1,16 @@
 export const BASE_URL = "http://10.0.0.70:8080";
+
+//AUTHENTICATION URLS
 export const LOGIN_URL = BASE_URL + '/login';
+export const SIGNUP_URL = BASE_URL + '/signup';
+
+//ROOM URLS
+export const CREATE_ROOM_URL = BASE_URL + '/room/create';
+export const JOIN_ROOM_URL = BASE_URL + '/room/join';
+export const LEAVE_ROOM_URL = BASE_URL + '/room/leave';
+export const MEMBERS_OF_ROOM_URL = BASE_URL + '/room/members';
+export const TASKS_OF_ROOM_URL = BASE_URL + '/room/tasks';
+
+
+//TASK URLS
+export const TASK_CREATE_URL = BASE_URL + '/task/create';
---
+++ room-ease/src/containers/Homepage/Homepage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Layout from '../../hoc/Layout/Layout';
+
+
+class Homepage extends Component {
+    render() {
+        return (
+            <Layout>
+
+
+
+            </Layout>
+        )
+    }
+
+}
+
+export default Homepage;
