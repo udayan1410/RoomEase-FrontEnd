7819d8d91f045b3a7645b72428d48054ed632951
udayan1410 <udayanbirajdar@gmail.com>
2020-10-05 14:32:49 -0700

    Create ClientRoutes.js

---
+++ room-ease/src/constants/ClientRoutes.js
@@ -0,0 +1,11 @@
+//Login and Authentication
+export const BASE_URL = "/";
+export const LOGIN_URL = "/login";
+export const SIGNUP_URL = "/signup";
+
+
+//Room URLS
+export const ROOM_URL = "/room";
+export const ROOM_JOIN_URL = ROOM_URL + "/join";
+export const ROOM_CREATE_URL = ROOM_URL + "/create";
+
