edc3004b88006728661feab162d9863aee6b1c4a
gauravgandhigg <gaurav.gandhi15698@gmail.com>
2020-10-16 15:40:52 -0700

    Added userprofile

--- room-ease/src/containers/UserProfile/UserProfile.js
+++ room-ease/src/containers/UserProfile/UserProfile.js
@@ -5,50 +5,70 @@ import { Link, withRouter } from "react-router-dom";
 
 import * as classes from './userprofile.module.css';
 import axios from 'axios';
-import { USER_PROFILE_URL,MEMBERS_OF_ROOM_URL } from '../../constants/ServerRoutes';
-
+import { USER_PROFILE_URL } from '../../constants/ClientRoutes';
+import {MEMBERS_OF_ROOM_URL} from '../../constants/ServerRoutes';
+import { withLayout } from '../../hoc/Layout/withLayout'
 
 class UserProfile extends Component{
     constructor(props){
     super(props);
-        
-    }
-    hasRoom= async ()=>{
-        let userID='5f79657944f5f348ac09d781'
-        let roomname="SCU"
-        let userStatus=(await axios.post(MEMBERS_OF_ROOM_URL, roomname)).data;
-        let { responseObject, user } = userStatus;
-        console.log(responseObject)
-
-        if (responseObject.Result === "Success")
-        {
-            return(
-                <div>
-                    <p>Hi</p>
-                </div>
-            )
+        this.state={
+            roomInfo:[],
+            userInfo:{},
         }
-        else
-            return null
+    }
+    
 
-
+    componentDidMount(){
+        this.hasRoom()
     }
 
+    hasRoom= async ()=>{
+        let userID=12312312
+        let roomName=localStorage.getItem('roomName')
+        console.log("adasd",userID,roomName)
+        let profileStatus= (await axios.get(MEMBERS_OF_ROOM_URL+"?roomname=SCU")).data;
+        let {Result,Error}= profileStatus
+        console.log("asdasasdasdas",profileStatus.Members)
+        this.setState({roomInfo:profileStatus.Members});
+        let obj={}
+        obj["userID"]=userID;
+        obj["roomName"]=roomName;
+        this.setState({userInfo:obj});
+        console.log(this.state.roomInfo);
+    }
+    check=()=>{
+        console.log("Adsdas")
+    }
 
-    
-    
     render(){
         return(
-        <div>
-
-            <h1>Hiass</h1>
-            {this.hasRoom}
-            
+        <div className={classes.container}>
+            <div className= {classes.left}>
+                {console.log("adasd",this.state.userInfo)}
+                <div className={classes.userprofilebox}>
+                        <img src={require('../../../src/assets/profile.png')} className={classes.profileimg}/>
+                </div>
+                <p>{this.state.userInfo.userID}</p>
+                <RegularButton text="Leave Room"></RegularButton>
+            </div>
+            <div className={classes.right}>
+                <h1>safds</h1>
+                {this.state.roomInfo.map((person, index) => (
+                <p key={index}>Hello, {person.userName} !</p>
+                ))}
+            </div>
         </div>
         );
-
-
-
+/*
+    2 subdivs
+    1 for left side
+           profile info
+           flexbox left
+           
+    1 for right side
+            member and roominfo
+*/
     }
 
 
@@ -56,6 +76,6 @@ class UserProfile extends Component{
 
 }
 
-export default withRouter(UserProfile);
+export default withLayout(UserProfile);
 
 
