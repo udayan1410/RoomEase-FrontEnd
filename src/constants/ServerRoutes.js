55e210fab1e036d4fd3285eb2241d0625f82a438
udayan1410 <udayanbirajdar@gmail.com>
2020-12-01 21:07:25 -0800

    Changed Server URL

--- src/constants/ServerRoutes.js
+++ src/constants/ServerRoutes.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://quiet-cliffs-40319.herokuapp.com";
-export const BASE_URL = "http://10.0.0.70:8080";
+export const BASE_URL = "https://quiet-cliffs-40319.herokuapp.com";
+// export const BASE_URL = "http://10.0.0.70:8080";
 
 //AUTHENTICATION URLS
 export const LOGIN_URL = BASE_URL + '/login';
